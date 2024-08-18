3c1dd171a316e1e42dd959e099884b4f29d5e9c2
hsbakir <94084650+DEVBakir@users.noreply.github.com>
2024-08-18 23:06:53 +0100

    Add Letest Section (Not Responsive)

--- frontend/src/Components/RecentBlog.jsx
+++ frontend/src/Components/RecentBlog.jsx
@@ -1,12 +1,10 @@
 function RecentBlog({ title, date, desc, img }) {
   return (
-    <div className="border-b-4 hover:border-[#F6C90E] border-b-transparent transition-all ease-in-out">
-      <img src={img} className="rounded-t-sm h-[255px]" />
-      <div className="px-5 py-3 flex flex-col justify-between h-[280px] border border-[#3a475029]">
+    <div className="recentBlog border-b-4 hover:border-[#F6C90E] border-b-transparent transition-all ease-linear">
+      <img src={img} className="rounded-t-md h-[255px] w-full" />
+      <div className="px-5 py-3 flex flex-col justify-between h-[280px] border border-[#3a475029] rounded-b-md">
         <h4 className="font-[500] text-lg text-[#303841]">{title}</h4>
-        <p className="">
-          {desc}
-        </p>
+        <p className="">{desc}</p>
         <span>{date}</span>
       </div>
     </div>
--- frontend/src/Pages/Blog.jsx
+++ frontend/src/Pages/Blog.jsx
@@ -2,12 +2,55 @@ import GalleryItem from "../Components/GalleryItem";
 import BlogImg3 from "../assets/BlogImg3.png";
 import BlogImg2 from "../assets/BlogImg2.png";
 import BlogImg1 from "../assets/BlogImg1.png";
+import Article1 from "../assets/Blog2.png";
+import Article2 from "../assets/girl.png";
+import Article3 from "../assets/article3.png";
+import RecentBlog from "../Components/RecentBlog";
 
 function Blog() {
+  const articles = [
+    {
+      title: "How to Use and Incorporate Symbolism in Photography",
+      date: "August 04, 2024",
+      img: Article1,
+      desc: "Symbolism in photography utilizes visual elements to convey abstract emotions or concepts, adding new levels of meaning and depth.",
+    },
+    {
+      title: "Understanding the Rule of Thirds",
+      date: "August 05, 2024",
+      img: Article2,
+      desc: "Are you thinking about becoming a portrait photographer? Well, you might like to read about these 16 types of portrait photography.",
+    },
+    {
+      title: "The Art of Black and White Photography",
+      date: "August 06, 2024",
+      img: Article3,
+      desc: "A comprehensive look at the rules of photography composition with photo examples and how to apply these rules.",
+    },
+    {
+      title: "Exploring Landscape Photography Techniques",
+      date: "August 07, 2024",
+      img: Article1,
+      desc: "Symbolism in photography utilizes visual elements to convey abstract emotions or concepts, adding new levels of meaning and depth.",
+    },
+    {
+      title: "Mastering Portrait Photography",
+      date: "August 08, 2024",
+      img: Article2,
+      desc: "Are you thinking about becoming a portrait photographer? Well, you might like to read about these 16 types of portrait photography.",
+    },
+    {
+      title: "Capturing Motion in Photography",
+      date: "August 09, 2024",
+      img: Article3,
+      desc: "A comprehensive look at the rules of photography composition with photo examples and how to apply these rules.",
+    },
+  ];
+
   return (
     <div className="mx-[150px] py-10">
       {/* Gallery Articles */}
-      <div className="gallery h-[550px] flex w-full gap-5">
+      <div className="gallery h-[550px] py-[10px] flex w-full gap-5">
         <GalleryItem
           image={BlogImg1}
           title="How to Become a Wildlife Photographer"
@@ -27,6 +70,23 @@ function Blog() {
             width="w-full"
             height="h-full"
           />
+        </div>
+      </div>
+      {/* Latest Article */}
+      <div className="pt-[110px]">
+        <h3 className="font-medium text-[32px] text-black text-center mb-[110px]">
+          Latest Articles
+        </h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 round">
+          {articles.map((article, index) => (
+            <RecentBlog
+              key={index}
+              title={article.title}
+              date={article.date}
+              img={article.img}
+              desc={article.desc}
+            />
+          ))}
         </div>
       </div>
     </div>
